4500e490e9802bac3a128327c9233e476a1aba05
G9 Aerospace <g9militants@gmail.com>
2023-10-08 21:57:47 +0530

    made the code more advanced

--- bot.py
+++ bot.py
@@ -4,10 +4,11 @@ import os
 import asyncio
 import wikipediaapi
 import logging
-from dotenv import load_dotenv  # Import load_dotenv from dotenv
+from dotenv import load_dotenv
+import cachetools
 
 # Load environment variables from .env file
-load_dotenv()  # Load .env variables at the beginning of your script
+load_dotenv()
 
 # Get the bot token from the environment variables
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -25,12 +26,15 @@ bot = commands.Bot(command_prefix='/', intents=intents)
 
 # Create a Wikipedia API client with a user agent
 wiki_wiki = wikipediaapi.Wikipedia(
-    language='en',  # Specify the Wikipedia language (e.g., 'en' for English)
-    user_agent="SmartGuy/1.0",  # Set your bot's name and version as the user agent
+    language='en',
+    user_agent="SmartGuy/1.0",
 )
 
 # Dictionary to store the listening channels for each guild
 listening_channels = {}
+
+# Create a cache for Wikipedia API responses
+cache = cachetools.LRUCache(maxsize=100)  # You can adjust the cache size as needed
 
 # Event handler for when the bot is ready
 @bot.event
@@ -46,7 +50,7 @@ async def on_ready():
     description="Set the channel where the bot should listen.",
 )
 async def set_channel(ctx, channel: discord.TextChannel):
-    # Check if the user has the 'manage_channels' permission (guild owner typically has it)
+    # Check if the user has the 'manage_channels' permission
     if ctx.author.guild_permissions.manage_channels:
         # Store the channel ID in the dictionary
         listening_channels[ctx.guild.id] = channel.id
@@ -64,14 +68,14 @@ def load_listening_channels():
     try:
         with open('listening_channels.txt', 'r') as file:
             data = file.read()
-            listening_channels = eval(data)  # Load as a dictionary
+            listening_channels = eval(data)
     except FileNotFoundError:
-        listening_channels = {}  # Initialize as an empty dictionary
+        listening_channels = {}
 
 # Function to save listening channels to a file
 def save_listening_channels():
     with open('listening_channels.txt', 'w') as file:
-        file.write(str(listening_channels))  # Save as a string representation of the dictionary
+        file.write(str(listening_channels))
 
 # Function to send messages with pagination buttons and edit the existing message
 async def send_paginated_messages(channel, messages):
@@ -80,41 +84,52 @@ async def send_paginated_messages(channel, messages):
     message = None
 
     while True:
-        # Create an embed with the current page content
         embed = discord.Embed(
             title=f'Page {current_page + 1}/{total_pages}',
             description=messages[current_page],
             color=discord.Color.blue()
         )
 
-        # Edit the existing message or send a new one if it doesn't exist
         if message:
             await message.edit(embed=embed)
         else:
             message = await channel.send(embed=embed)
 
-        # Add reaction buttons
         if total_pages > 1:
             await message.add_reaction('◀️')  # Backward
             await message.add_reaction('▶️')  # Forward
+            await message.add_reaction('🔢')  # Go to page
 
         try:
+            def check(reaction, user):
+                return reaction.message.id == message.id and user != bot.user
+
             reaction, user = await bot.wait_for(
                 'reaction_add',
                 timeout=60.0,
-                check=lambda r, u: r.message.id == message.id and u != bot.user
+                check=check
             )
 
             if str(reaction) == '▶️' and current_page < total_pages - 1:
                 current_page += 1
             elif str(reaction) == '◀️' and current_page > 0:
                 current_page -= 1
+            elif str(reaction) == '🔢':
+                await message.remove_reaction('🔢', user)
+                await user.send(f"Please enter a page number (1-{total_pages}):")
 
-            # Remove the user's reaction
+                def page_number_check(m):
+                    return m.author == user and m.content.isdigit() and 1 <= int(m.content) <= total_pages
+
+                try:
+                    response = await bot.wait_for('message', check=page_number_check, timeout=30.0)
+                    current_page = int(response.content) - 1
+                except asyncio.TimeoutError:
+                    await user.send("You took too long to respond. Page selection canceled.")
+
             await message.remove_reaction(reaction, user)
 
         except asyncio.TimeoutError:
-            # Stop pagination after a minute of inactivity
             break
 
         await asyncio.sleep(1)  # Add a slight delay to prevent API abuse
@@ -122,11 +137,17 @@ async def send_paginated_messages(channel, messages):
 # Function to research the user's message (fetch information from Wikipedia and send a reply with pagination)
 async def research_message(query, channel):
     try:
-        # Use the Wikipedia API to fetch information
-        page = wiki_wiki.page(query)
+        # Check if the response is cached
+        if query in cache:
+            page = cache[query]
+        else:
+            # Use the Wikipedia API to fetch information
+            page = wiki_wiki.page(query)
+
+            # Cache the response for 5 minutes
+            cache[query] = page
 
         if page.exists():
-            # Split the Wikipedia page content into chunks based on sentences
             sentences = page.text.split(". ")
             chunks = []
             current_chunk = ""
@@ -141,7 +162,6 @@ async def research_message(query, channel):
             if current_chunk:
                 chunks.append(current_chunk)
 
-            # Send messages with pagination
             await send_paginated_messages(channel, chunks)
         else:
             await channel.send("No information found on Wikipedia.")
@@ -153,20 +173,40 @@ async def research_message(query, channel):
 @bot.event
 async def on_message(message):
     if message.author == bot.user:
-        return  # Ignore messages sent by the bot itself
+        return
 
     if message.guild:
-        # Check if the message is in the listening channel for the guild
         guild_id = message.guild.id
         if guild_id in listening_channels:
             listening_channel_id = listening_channels[guild_id]
             if message.channel.id == listening_channel_id:
-                # Your message processing logic here
                 await research_message(message.content, message.channel)
 
-    # Allow other event handlers to process the message
     await bot.process_commands(message)
 
+# Command error handling
+@set_channel.error
+async def set_channel_error(ctx, error):
+    if isinstance(error, commands.BadArgument):
+        await ctx.send("Invalid channel. Please mention a valid text channel.")
+    elif isinstance(error, commands.MissingPermissions):
+        await ctx.send("You don't have permission to use this command.")
+    else:
+        await ctx.send(f"An error occurred: {str(error)}")
+
+# Custom help command
+bot.remove_command('help')
+
+@bot.command(
+    name="help",
+    description="Show help and usage information for commands.",
+)
+async def custom_help(ctx):
+    help_message = "Available commands:\n" \
+                   "/setchannel <channel_mention>: Set the bot's listening channel.\n" \
+                   "/help: Show this help message."
+
+    await ctx.send(help_message)
 
 # Run the bot with your token
-bot.run(TOKEN)  # Use the TOKEN variable
+bot.run(TOKEN)
